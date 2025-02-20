b917daf2c3a85f9fcc214a8455d7c810d05446c4
Zagoman <lucaszago.ph@gmail.com>
2025-02-20 20:43:38 +0100

    increase which-key timeout and fix go lsp

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -157,7 +157,6 @@ return {
 								rangeVariableTypes = true,
 							},
 							analyses = {
-								fieldalignment = true,
 								nilness = true,
 								unusedparams = true,
 								unusedwrite = true,
